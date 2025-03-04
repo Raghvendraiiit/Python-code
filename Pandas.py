fb18b90917ff81ec1ef47e15735a2427da4083d6
Raghvendra-iiit <raghiiitbang@gmail.com>
2025-03-04 19:44:50 +0530

    second max

--- Pandas.py
+++ Pandas.py
@@ -26,4 +26,17 @@ def sort(l1):
                 l1[j], l1[j+1] = l1[j+1], l1[j]
     return l1
 sort(l1)
-        
+##############################################################################
+
+def second_min(x):
+    min_1 = min_2 = float('inf')
+    for i in range(len(x)):
+        if x[i] < min_1:
+            min_2 = min_1
+            min_1 = x[i]
+        elif min_1 < x[i] < min_2:
+            min_2 = x[i]
+
+    return min_2
+
+second_min([1,3,8,2,7,3,6,8])        
