297a99a26b0295a221536eda33d369018b617e8e
Raghvendra-iiit <raghiiitbang@gmail.com>
2025-03-04 19:39:05 +0530

    add sorting code

--- Pandas.py
+++ Pandas.py
@@ -5,7 +5,7 @@ print(array_1)
 
 array_2 = np.array([[1,2,3,4],[5,6,7,8]])
 print(array_2)
-
+##########################################################
 l1 = [2,6,7,8,4]
 def first_max(l1):
     max_1 = float('-inf')
@@ -14,5 +14,16 @@ def first_max(l1):
             max_1 = i
     return max_1
 first_max(l1)
+################################
+        
+# Sort the array without use any prebuilt function..
+l1 = [2,4,9,7,5,4,6,7,4,2]
 
+def sort(l1):
+    for i in range(len(l1)):
+        for j in range(0, len(l1)-i-1):
+            if l1[j] > l1[j+1]:
+                l1[j], l1[j+1] = l1[j+1], l1[j]
+    return l1
+sort(l1)
         
