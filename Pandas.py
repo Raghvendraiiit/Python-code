7d8639f6bdcc93ae84828113cc6e329d2bc4b5f7
Raghvendra-iiit <raghiiitbang@gmail.com>
2025-03-04 19:32:39 +0530

    add first_max

--- Pandas.py
+++ Pandas.py
@@ -5,3 +5,14 @@ print(array_1)
 
 array_2 = np.array([[1,2,3,4],[5,6,7,8]])
 print(array_2)
+
+l1 = [2,6,7,8,4]
+def first_max(l1):
+    max_1 = float('-inf')
+    for i in l1:
+        if i > max_1:
+            max_1 = i
+    return max_1
+first_max(l1)
+
+        
